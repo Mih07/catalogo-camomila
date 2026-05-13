5e637b9be542ec6158cf4d06975e9fba62ed3c53
Michele Santo <micheleespsanto@gmail.com>
2026-05-13 18:31:13 -0300

    ajuste

--- script.js
+++ script.js
@@ -87,6 +87,8 @@ function renderizarProdutos(lista) {
     const v = document.getElementById('vitrine');
     const vDestaques = document.getElementById('vitrine-destaque');
     const bannerPrincipal = document.getElementById('banner-principal');
+    const vPromo = document.getElementById('vitrine-promocao');
+
     if (!v) return;
 
     // 1. Busca e exibe o banner
@@ -94,7 +96,7 @@ function renderizarProdutos(lista) {
     if (banner && banner.img && bannerPrincipal) {
         bannerPrincipal.innerHTML = `
             <div class="px-1 py-2" style="background:#f1f8f1; border-radius:0 0 8px 8px; border-bottom:1px solid #e1eee1;">
-                <img src="${banner.img}" class="img-fluid" style="width: 100%; height: 350px; object-fit: cover;" alt="Banner Camomila">
+                <img src="${banner.img}" class="img-fluid" style="width: 100%; height: 550px; object-fit: cover;" alt="Banner Camomila">
             </div>
         `;
     }
